c1aae249293f6ef50426db627b6e311713f2d41f
Корюков Олег <kovprog@gmail.com>
2017-12-17 13:43:39 +1100

    Добавьте файлы проекта.

---
+++ Delanay/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using D = System.Drawing;
+namespace Delanay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static int numPoints = 1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+          
+
+            numPoints++;
+            List<Point> points = new List<Point>();
+
+            Random rand = new Random(System.Environment.TickCount);
+
+            Graphics g = this.CreateGraphics();
+          
+            g.Clear(Color.White);
+
+            float doubleRadius = Point.pointRadius*2;//Чтоб точки не уходили за экран
+
+
+            bool Intersect = false;
+            int numIntersect = 0;
+            while (points.Count< numPoints)
+            {
+                Point newGeneratedPoint = new Point(rand.Next((int)( doubleRadius), 800-(int)(doubleRadius) ), rand.Next((int)( doubleRadius), 600-(int)(doubleRadius))) ;
+
+               
+                foreach(var t in points)
+                {
+
+                    float distance = t.DistanceToPoint(newGeneratedPoint);
+
+                    if ((distance -Point.pointRadius*2)<=0)
+                    {
+                        numIntersect++;
+                        Intersect = true;
+                        break;
+                    }
+
+                    
+
+                }
+
+                if (Intersect == true)
+                {
+                   
+                    Intersect = false;
+                    continue;
+                }
+
+                points.Add(newGeneratedPoint);
+            }
+
+            foreach (var t in points)
+            {
+                t.Draw(g);
+            }
+            points.Sort();
+
+            
+           
+            for(int i=0;i<points.Count-1;i++)
+            {
+                g.DrawLine(Pens.RoyalBlue, new  D.Point((int)points[i].X, (int)points[i].Y), new D.Point((int)points[i+1].X, (int)points[i+1].Y));
+            }
+
+
+            
+
+           
+
+
+
+
+
+        }
+    }
+}
---
+++ Delanay/Point.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delanay
+{
+    public class Point :IComparable
+    {
+        public static float pointRadius = 10;
+        float x;
+        float y;
+
+        public Point (float _x,float _y)
+        {
+            X = _x;
+            Y = _y;
+        }
+
+        public float X {
+
+            get {
+                return x;
+            }
+            set {
+                x = value;
+            }
+        }
+        public float Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+
+        public void Draw(System.Drawing.Graphics g)
+        {
+            g.DrawEllipse(System.Drawing.Pens.Black, X-pointRadius, Y-pointRadius, pointRadius*2, pointRadius*2);
+        }
+
+
+        public float DistanceToPoint(Point p )
+        {
+            float len = 0;
+
+            len = (float)Math.Sqrt((p.x-this.x ) * (p.x-this.x ) + (p.y-this.y) * (p.y-this.y));
+
+
+            return len;
+        }
+
+
+
+        public int CompareTo(object obj)
+        {
+
+            if (X < ((Point)obj).X)
+            {
+                return -1;
+            }
+
+            if (X > ((Point)obj).X)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
